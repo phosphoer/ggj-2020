c82d7d6b6c1453a45866681177cb522f5561e870
David Evans <david0evans@gmail.com>
2020-01-31 22:40:06 +0000

    create main camera prefab

---
+++ ggj-2020/Assets/Game/Scripts/AstronautController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class AstronautController : MonoBehaviour
+{
+  public Vector3 MoveVector
+  {
+    get { return _moveVector; }
+    set
+    {
+      _moveVector = Vector3.ClampMagnitude(value, 1);
+    }
+  }
+
+  private Vector3 _moveVector;
+
+  private void Update()
+  {
+  }
+}
